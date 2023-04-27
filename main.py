76f080fc612219d281ac6d3efc9b7c7b01f41286
Nikolay Chernikov <nikolay.chernikov.dev@gmail.com>
2023-04-27 10:16:53 +0300

    feat: include deposit router to fastapi app

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import uvicorn
 from fastapi import FastAPI
+from deposit_router import router as deposit_router
 
 app = FastAPI()
+app.include_router(deposit_router)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
